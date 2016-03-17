53ce70038d01ff583737a186d264e03c81fc1638
Tieme-Frederic Togola <ttogola@uwaterloo.ca>
2016-03-16 23:03:30 -0400

    update every interval

--- plot.js
+++ plot.js
@@ -1,24 +1,17 @@
 var sql = require('sql.js'),
     fs = require('fs');
 
-var refreshInterval = 3000;
+var refreshInterval = 5000;
 var db = new sql.Database(fs.readFileSync('../test.sqlite'));
 // TODO: take care of different column names, SELECT ... AS to convert
 //var tableInfo = 'PRAGMA table_info(' + tables[i] + ')';
 // ignore invalid data, just small notification
 // try to handle various database schema, take user input to help
-
-// retrieve table names
-var tables = [];
-var tablesQuery = 'SELECT tbl_name FROM sqlite_master WHERE type="table"';
-db.each(tablesQuery, {}, (row) => {
-  tables.push(row.tbl_name);
-});
+// triggers?
 
 function buildUnionQuery(column, tables, whereCond) {
   var query = 'SELECT ' + column + ' FROM ';
   var whereStr = (whereCond === undefined) ? '' : ' WHERE ' + whereCond;
-
   tables.forEach((table, index) => {
     if (index == tables.length-1)
       query += table + whereStr + ';';
@@ -27,33 +20,40 @@ function buildUnionQuery(column, tables, whereCond) {
   return query;
 }
 
-var metrics = [];
-var namesQuery = buildUnionQuery('DISTINCT name', tables);
-
-db.each(namesQuery, {}, (metric) => {
-  // plotly format: metric = {name:name, x:timestamps, y:values, type:type}
-  var dataQuery = buildUnionQuery('timestamp, value', tables,
-                                  'name="' + metric.name + '"');
-  var data = db.exec(dataQuery)[0].values;
-  metric.x = []; metric.y = [];
-  data.forEach((element) => {
-    metric.x.push(element[0]);
-    metric.y.push(element[1]);
+function collectData() {
+  // retrieve table names
+  var tables = [];
+  var tablesQuery = 'SELECT tbl_name FROM sqlite_master WHERE type="table"';
+  db.each(tablesQuery, {}, (row) => {
+    tables.push(row.tbl_name);
   });
-  metric.type = 'scatter';
-  metrics.push(metric);
-});
+
+  var metrics = [];
+  var namesQuery = buildUnionQuery('DISTINCT name', tables);
+
+  db.each(namesQuery, {}, (metric) => {
+    // plotly format: metric = {name:name, x:timestamps, y:values, type:type}
+    var dataQuery = buildUnionQuery('timestamp, value', tables,
+                                    'name="' + metric.name + '"');
+    var data = db.exec(dataQuery)[0].values;
+    metric.x = []; metric.y = [];
+    data.forEach((element) => {
+      metric.x.push(element[0]);
+      metric.y.push(element[1]);
+    });
+    metric.type = 'scatter';
+    metrics.push(metric);
+  });
+
+  return metrics;
+}
 
 var layout = {displayModeBar: true, scrollZoom: true};
-Plotly.newPlot('plotdiv', metrics, null, layout);
+Plotly.newPlot('plotdiv', collectData(), null, layout);
 
-//setInterval(() => {
-  //var d = plotdiv.data;
-  //var len = d[0].x.length;
-  //d[0].x.push(d[0].x[len-1]+1);
-  //d[0].y.push(d[0].y[len-1]+1);
-  //d[0].type = 'scatter';
-  //plotdiv.data = d;
-  //Plotly.redraw('plotdiv'); // use more efficient update
-//}, refreshInterval);
-// triggers?
+setInterval(() => {
+  plotdiv.data = collectData();
+  Plotly.redraw('plotdiv');
+  // TODO: use more efficient update
+  //        redraw breaks graph view state (zoom, pan, ...)
+}, refreshInterval);
